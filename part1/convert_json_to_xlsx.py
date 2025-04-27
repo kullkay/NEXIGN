42ae8ff2132215b063a5f690a5eec5fe5b998a84
kullkay <ABulat2003@gmail.com>
2025-04-28 01:18:17 +0300

    Corrected part1

--- part1/convert_json_to_xlsx.py
+++ part1/convert_json_to_xlsx.py
@@ -3,11 +3,11 @@ import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 
-# Загружаем Trivy-отчёт
+
 with open("portainer.json", "r") as f:
     data = json.load(f)
 
-# Собираем уязвимости
+
 vulns = []
 for result in data.get("Results", []):
     for vuln in result.get("Vulnerabilities", []):
@@ -20,23 +20,19 @@ for result in data.get("Results", []):
 
 df = pd.DataFrame(vulns)
 
-# Сортируем по VulnerabilityID
 grouped_df = pd.DataFrame(vulns).sort_values(by=["PkgName", "VulnerabilityID"])
 
-# Путь к файлу
-filename = "portainer_vulns_grouped.xlsx"
 
-# Сохраняем в Excel
+filename = "portainer.xlsx"
+
 grouped_df.to_excel(filename, index=False)
 
-# Загружаем рабочую книгу для форматирования
 wb = load_workbook(filename)
 ws = wb.active
 
-# Устанавливаем ширину столбцов по максимальной длине в каждой колонке
 for col in ws.columns:
     max_length = 0
-    column = col[0].column  # Номер колонки (1, 2, 3…)
+    column = col[0].column
     for cell in col:
         try:
             if cell.value:
@@ -46,6 +42,4 @@ for col in ws.columns:
     adjusted_width = max_length + 2
     ws.column_dimensions[get_column_letter(column)].width = adjusted_width
 
-# Сохраняем результат
 wb.save(filename)
-print(f"✅ Уязвимости сохранены и отформатированы в {filename}")
